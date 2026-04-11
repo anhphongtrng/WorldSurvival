9d4795c430f07236eba95988073032d3db1a4c5d
anhphongtrng <182899977+anhphongtrng@users.noreply.github.com>
2026-04-11 19:24:21 +0700

    add Weapon Controller
    - Weapon Controller can switch weapon

--- Assets/Scripts/Player/Weapon/WeaponController.cs
+++ Assets/Scripts/Player/Weapon/WeaponController.cs
@@ -1,16 +1,44 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class WeaponController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] GameObject beamObject;
+    [SerializeField] GameObject gunObject;
+
+    GameObject currentWeapon;
+
+    private void Awake()
     {
-        
+        gunObject = GameObject.Find("Gun");
+        gunObject.SetActive(false);
+        beamObject = GameObject.Find("Beam");
+        beamObject.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        SwitchWeapon();
+    }
+
+    public void SwitchWeapon()
+    {
+        if(Keyboard.current.digit1Key.wasPressedThisFrame)
+        {
+            EquipWeapon(gunObject);
+        }
+        else if(Keyboard.current.digit2Key.wasPressedThisFrame)
+        {
+            EquipWeapon(beamObject);
+        }
+    }
+
+    public void EquipWeapon(GameObject newWeapon)
+    {
+        if (currentWeapon != null)
+            currentWeapon.SetActive(false);
+
+        currentWeapon = newWeapon;
+        currentWeapon.SetActive(true);
     }
 }
